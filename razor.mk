3134942a54174e85d54f64df76ff1d30f446b67e
vibhu0009 <cvibhu0009@gmail.com>
2015-09-26 09:34:41 +0200

    Make it Razor

--- razor.mk
+++ razor.mk
@@ -1,13 +1,13 @@
 $(call inherit-product, device/lge/vs985/full_vs985.mk)
-$(call inherit-product, vendor/slim/config/common_full_phone.mk)
-$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/razor/config/common_full_phone.mk)
+$(call inherit-product, vendor/razor/config/nfc_enhanced.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1440
 TARGET_SCREEN_HEIGHT := 2560
 
 PRODUCT_RELEASE_NAME := LG G3
-PRODUCT_NAME := slim_vs985
+PRODUCT_NAME := razor_vs985
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="g3" \
